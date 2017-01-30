edaece904496f981ffd09acf21a0034e1221d7e0
Charaka Wijesuriya <charaka@Charakas-MacBook-Pro-2.local>
2017-01-30 21:18:26 +1100

    Finishing up

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,15 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import allReducers from './reducers'
+import App from './components/app'
 
-ReactDOM.render(<h1>Hey Now</h1>,document.getElementById('root'));
+const store = createStore(allReducers);
+
+
+ReactDOM.render(<Provider store={store}>
+                    <App />
+                </Provider>
+                ,document.getElementById('root'));
---
+++ dev/js/reducers/reducer-user.js
@@ -0,0 +1,32 @@
+export default function(){
+    return[
+        {
+            id: 1,
+            first: "charaka",
+            last: "wijesuriya",
+            age: 62,
+            description: "Charaka is learnig react",
+            thumbnail: "http://i.imgur.com/VTqEa02.png"
+
+        },
+        {
+            id: 2,
+            first: "anu",
+            last: "wije",
+            age: 67,
+            description: "Anu is learnig Marketing",
+            thumbnail: "http://i.imgur.com/MO7oK8B.png"
+
+        },
+        {
+            id: 3,
+            first: "Madawa",
+            last: "wije",
+            age: 40,
+            description: "Madhaws is learnig Driving",
+            thumbnail: "http://i.imgur.com/5awz6wG.gif"
+
+        },
+        
+    ]
+}
